c8adea5cf8908667124629659661548e27f299bd
roycejewell <connect@brandonjewell.com>
2023-10-16 18:18:00 -0600

    init

---
+++ src/components/faq.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+const Faq = () => {
+  return (
+  	<div className={'border rounded-xl p-[20px] lg:p-[40px] overflow-hidden'}>
+  		<h2>Question Here</h2>
+  		<div className={''}>
+  			<p>For an extra cherry (or two!) on top, I’d like to add a couple of CSS transitions to make the content roll out slowly, as well as a rotating caret symbol on the label. See the code below for how I’ve implemented this:</p>
+  		</div>
+  	</div>
+  )
+}
+
+export default Faq
